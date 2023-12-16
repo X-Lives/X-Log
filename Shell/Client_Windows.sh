e7255a129240e9e27590c828077cd7ae8f83830e
is52hertz <is.52hertz@gmail.com>
2023-12-16 15:21:45 +0800

    Shell Woops : Forgot to submit the commit on `X-bin`. This is a commit that `git push` was forgotten about. Perform compile-time modifications to `Shell` after creation of `X-bin`. —————— Shell Woops : 忘记提交关于 `X-bin` 的commit。 这是一个忘记 `git push` 的提交。 对于 `X-bin` 的创建后 `Shell` 执行编译时的修改。

--- Shell/Client_Windows.sh
+++ Shell/Client_Windows.sh
@@ -3,61 +3,105 @@
 source "variables.sh"
 
 cd ../..
-cd "${directory_main}/build"
-source "variables-build.sh"
-cd ../..
-# SDL lib for linux-windows cross compile
 
-if [ ! -e SDL-1.2.15 ]
-then
-    wget https://www.libsdl.org/release/SDL-devel-1.2.15-mingw32.tar.gz
-    tar -xvzf SDL-devel-1.2.15-mingw32.tar.gz
-    rm SDL-devel-1.2.15-mingw32.tar.gz
-fi
+# prefix
+## SDL lib for linux-windows cross compile
+# cd ${directory_bin}
+
+# if [ ! -e SDL-1.2.15 ]
+# then
+#     wget https://www.libsdl.org/release/SDL-devel-1.2.15-mingw32.tar.gz
+#     tar -xvzf SDL-devel-1.2.15-mingw32.tar.gz
+#     rm SDL-devel-1.2.15-mingw32.tar.gz
+# fi
 
 if [ ! -e SDL ]
 then
-    ln -s SDL-1.2.15/include/SDL .
+    ln -s ${directory_bin}/Windows/SDL-1.2.15/include/SDL .
 fi
-
-# freetype lib for linux-windows cross compile
-if [ ! -e mingw32 ]
-then
-    wget https://mirror.msys2.org/mingw/mingw32/mingw-w64-i686-freetype-2.13.2-1-any.pkg.tar.zst
-    tar --use-compress-program=unzstd -xvf mingw-w64-i686-freetype-2.13.2-1-any.pkg.tar.zst
-    rm mingw-w64-i686-freetype-2.13.2-1-any.pkg.tar.zst
-fi
+## freetype lib for linux-windows cross compile
+# if [ ! -e mingw32 ]
+# then
+#     wget https://mirror.msys2.org/mingw/mingw32/mingw-w64-i686-freetype-2.13.2-1-any.pkg.tar.zst
+#     tar --use-compress-program=unzstd -xvf mingw-w64-i686-freetype-2.13.2-1-any.pkg.tar.zst
+#     rm mingw-w64-i686-freetype-2.13.2-1-any.pkg.tar.zst
+# fi
 
 if [ ! -e freetype2 ]
 then
-    ln -s mingw32/include/freetype2 .
+    ln -s ${directory_bin}/Windows/mingw32/include/freetype2 .
 fi
 
 if [ ! -e Winsock.h ]
 then
     ln -s /usr/i686-w64-mingw32/include/winsock.h Winsock.h
 fi
+## other lib for linux-windows cross compile
+if [ ! -e libpng16 ]
+then
+    ln -s ${directory_bin}/Windows/mingw32/include/libpng16 .
+fi
+if [ ! -e zlib.h ]
+then
+    ln -s ${directory_bin}/Windows/mingw32/include/zlib.h .
+fi
 
+# cp -rf ${directory_bin}/Windows/mingw32/* . 
+## Download `X-bin` before 2023/12/16 15:00 to perform compilation, please uncomment this code.
+## And comment out the previous code - starting with `if [ ! -e SDL ]`.
 
-cd "${directory_main}"
+cd ${directory_main}
 
+chmod u+x configure
 ./configure 5
 cd gameSource
 make
+sh makeEditor.sh
+sh makeEditor-zh_cn.sh
 
-cd ../build
+cd ../..
+mkdir ${directory_win}
+cd ${directory_win}
 
-./makeDistributionWindows
+cp ../${directory_main}/gameSource/${client_name} .
+cp ../${directory_main}/gameSource/${editor_name} .
+cp ../${directory_main}/gameSource/${editor_name}-zh_cn .
+cp -rf ../${directory_main}/gameSource/graphics .
+cp -rf ../${directory_main}/gameSource/languages .
+cp -rf ../${directory_main}/gameSource/settings-client settings
+cp -rf ../${directory_main}/gameSource/settings-client .
+cp -rf ../${directory_main}/gameSource/settings-editor .
+cp ../${directory_main}/gameSource/reverbImpulseResponse.aiff .
+cp ../${directory_main}/gameSource/wordList.txt .
+cp -rf "../${directory_data}/animations" .
+cp -rf "../${directory_data}/categories" .
+cp -rf "../${directory_data}/ground" .
+cp -rf "../${directory_data}/music" .
+cp -rf "../${directory_data}/objects" .
+cp -rf "../${directory_data}/sounds" .
+cp -rf "../${directory_data}/sprites" .
+cp -rf "../${directory_data}/transitions" .
+cp "../${directory_data}/dataVersionNumber.txt" .
+# cp "../${directory_main}/X-client_Version.txt" .
+cp "../${directory_main}/${directory_main_v_f}" .
+cp "../${directory_data}/${directory_data_v_f}" "${directory_data_2_v_f}"
 
-cp ../scripts/skps2010Scripts/translator.py "windows/${client_name}_v${Client_v}"
-cp ../scripts/skps2010Scripts/translator.exe "windows/${client_name}_v${Client_v}"
+rm -f settings/email.ini
+rm -f settings/accountKey.ini
 
-mv "windows/${client_name}_v${Client_v}" "../../"
+find . -type f -name '*~' -exec rm {} +
 
-cd ../../
+cp ../${directory_main}/scripts/skps2010Scripts/translator.py .
+cp ../${directory_main}/scripts/skps2010Scripts/translator.exe .
 
-echo "done building ${client_name}_v${Client_v}"
+cp ../${directory_main}/win32/*.dll .
 
-zip -r -q ${client_name}_Windows_v${Client_v}.zip ${client_name}_v${Client_v}
+Client_v=$(cat "${directory_main_v_f}")
 
-echo "done zipping ${client_name}_Windows_v${Client_v}.zip"
+cd ..
+
+echo -e "${CW_o_1}${Client_v}"
+
+zip -r -q ${client_name}_Windows_v${Client_v}.zip "${directory_win}"
+
+echo -e "${CW_o_2}${Client_v}.zip"
